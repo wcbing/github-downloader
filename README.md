0af1d48c6757c250780f9d2c516e91e90ffe5d66
wcbing <cbing2002@outlook.com>
2025-02-09 18:52:18 +0800

    refactor: 下载目录名称、版本变量名称
    下载目录名称改为 name
    verison_tag 改为 releases_tag
    stripped_version 改为 version

--- README.md
+++ README.md
@@ -23,8 +23,8 @@ Usage:
     -t, --thread <number>   The number of concurrent download threads, default is 5
     -r, --recursive         Recursive create directory, file save path like: 
                             'https:/github.com/<user>/<repo>/releases/
-                            download/<version-tag>/<filename>'
-                            Default path like: '<user>__<repo>/<filename>'
+                            download/<releases_tag>/<filename>'
+                            Default path like: '<name>/<filename>'
     --dry-run               Dry run with HTTP head method (do not download)
 
 用法: 
@@ -35,8 +35,8 @@ Usage:
     -t, --thread <number>   并发下载线程数量，默认为 5
     -r, --recursive         递归的创建目录，文件保存路径: 
                             'https:/github.com/<user>/<repo>/releases/
-                            download/<version-tag>/<filename>'
-                            默认路径: '<user>__<repo>/<filename>'
+                            download/<releases_tag>/<filename>'
+                            默认路径: '<name>/<filename>'
     --dry-run               用 http 的 head 方法试运行（不下载文件）
 ```
 
@@ -63,13 +63,20 @@ Usage:
     "rustdesk": {
         "repo": "rustdesk/rustdesk",
         "file_list": [
-            "rustdesk-{version_tag}-x86_64.deb"
+            "rustdesk-{releases_tag}-x86_64.deb"
         ]
     },
     "draw.io": {
         "repo": "jgraph/drawio-desktop",
         "file_list": [
-            "drawio-amd64-{stripped_version}.deb"
+            "drawio-amd64-{version}.deb"
+        ]
+    },
+    // ...
+    "name": {
+        "repo": "user/repo",
+        "file_list": [
+            "name_{version}.deb"
         ]
     }
 }
@@ -77,17 +84,14 @@ Usage:
 
 说明：
 
-- `"repo": xxx`  
-github 仓库名，形如：user/repo。
+- `"name`  
+程序名称，唯一标识，会作为默认保存目录的名称。
+- `"repo": user/repo`  
+github 仓库地址，包括用户名和仓库名。
 - `"file_list": []`  
 文件名列表，可包含以下变量。
-- `{version_tag}`  
-用于代替文件名中和 Releases Tag 相同的部分。
-- `{stripped_version}`  
-用于一些 Tag 是 `v1.1.0`，但是文件名中是 `1.1.0` 的情况。
-
-## Todo
-
-- [x] `-p, --proxy <url>` 使用 Github 下载代理
-- [x] `-d, --dir <data_dir>` 指定仓库配置所在目录
-- [x] `-t, --thread <number>` 设置并发数量
+    - `{releases_tag}`  
+    用于代替文件名中和 Releases Tag 相同的部分。
+    - `{version}`  
+    版本号，用于代替文件名中和 Releases Tag 中**数字开头**的部分。  
+    例如一些 Tag 是 `v1.1.0`，但是文件名中是 `1.1.0` 的情况。
--- internal/github/check.go
+++ internal/github/check.go
@@ -12,72 +12,82 @@ import (
 )
 
 // 拼接得到文件名
-func replaceFileName(latestVersion, templates string) (fileName string) {
-	tmpname := strings.ReplaceAll(templates, "{stripped_version}", latestVersion[1:])
-	fileName = strings.ReplaceAll(tmpname, "{version_tag}", latestVersion)
-	return fileName
+func replaceFileName(releasesTag, file_template string) (fileName string) {
+	tmpname := strings.ReplaceAll(file_template, "{version}", releasesTag[1:])
+	fileName = strings.ReplaceAll(tmpname, "{releases_tag}", releasesTag)
+	return
 }
 
-func Check(name string, repo config.GithubRepo, localVersion string) (versionTag string) {
+// Check 检查仓库更新并下载新版本文件
+// name: 应用名称
+// repo: 仓库配置
+// localVersion: 本地记录的版本
+// 返回最新的 Releases Tag
+func Check(name string, repo config.GithubRepo, localVersion string) (releasesTag string) {
 	wg := sync.WaitGroup{}
 	thread := config.Thread
 	sem := make(chan struct{}, thread) // Semaphore
 
 	repoUrl := config.Proxy + "https://github.com/" + repo.Repo
-	versionTag = LatestVersionTag(repoUrl)
-	log.Printf("%s = %s\n", name, versionTag)
+	releasesTag = latestReleasesTag(repoUrl)
+	log.Printf("%s = %s\n", name, releasesTag)
 	// 判断是否需要更新
-	if localVersion != versionTag {
-		releasesDownloadUrl := repoUrl + "/releases/download"
-		// 确定本地文件目录并确保目录存在
-		var fileDir string
-		if config.Config["recursive"] {
-			fileDir = filepath.Join(config.OutputDir, releasesDownloadUrl, versionTag)
-		} else {
-			fileDir = filepath.Join(config.OutputDir, strings.ReplaceAll(repo.Repo, "/", "__"))
-		}
-		if _, err := os.Stat(fileDir); os.IsNotExist(err) {
-			if err := os.MkdirAll(fileDir, 0755); err != nil {
-				log.Print(err)
-			}
-		}
-		// 下载新版本文件
-		for _, templates := range repo.FileList {
-			fileName := replaceFileName(versionTag, templates)
-			fileUrl := fmt.Sprintf("%s/%s/%s", releasesDownloadUrl, versionTag, fileName)
-			filePath := filepath.Join(fileDir, fileName)
-			wg.Add(1)
-			go func(fileUrl, filePath string) {
-				defer wg.Done()
-				sem <- struct{}{}        // 获取许可
-				defer func() { <-sem }() // 释放许可
-				Download(fileUrl, filePath)
-			}(fileUrl, filePath)
-		}
-		// 判断是否是新添加应用
-		if localVersion == "" {
-			fmt.Printf("AddNew: %s (%s)\n", name, versionTag)
-		} else {
-			fmt.Printf("Update: %s (%s -> %s)\n", name, localVersion, versionTag)
-			// 删除旧版本文件
-			if !config.Config["dry-run"] && !config.Config["recursive"] {
-				// 对非 "recursive" 的，依次删除旧版本文件
-				for _, templates := range repo.FileList {
-					// 如果模板中不包含 {stripped_version} 或 {version_tag}，则不删除
-					if !strings.Contains(templates, "{stripped_version}") && !strings.Contains(templates, "{version_tag}") {
-						continue
-					}
-					oldFilePath := filepath.Join(fileDir, replaceFileName(localVersion, templates))
-					os.Remove(oldFilePath)
-				}
-			} else if !config.Config["dry-run"] && config.Config["recursive"] {
-				// 对 "recursive" 的，直接删除旧版本目录
-				oldFileDir := filepath.Join(config.OutputDir, releasesDownloadUrl, localVersion)
-				os.RemoveAll(oldFileDir)
-			}
+	if localVersion == releasesTag || releasesTag == "" {
+		return
+	}
+
+	releasesDownloadUrl := repoUrl + "/releases/download"
+
+	// 确定本地文件目录并确保目录存在
+	var fileDir string
+	if config.Config["recursive"] {
+		fileDir = filepath.Join(config.OutputDir, releasesDownloadUrl, releasesTag)
+	} else {
+		fileDir = filepath.Join(config.OutputDir, name)
+	}
+	if _, err := os.Stat(fileDir); os.IsNotExist(err) {
+		if err := os.MkdirAll(fileDir, 0755); err != nil {
+			log.Print(err)
 		}
 	}
+
+	// 下载新版本文件
+	for _, templates := range repo.FileList {
+		fileName := replaceFileName(releasesTag, templates)
+		fileUrl := fmt.Sprintf("%s/%s/%s", releasesDownloadUrl, releasesTag, fileName)
+		filePath := filepath.Join(fileDir, fileName)
+
+		wg.Add(1)
+		go func(fileUrl, filePath string) {
+			defer wg.Done()
+			sem <- struct{}{}        // 获取许可
+			defer func() { <-sem }() // 释放许可
+			Download(fileUrl, filePath)
+		}(fileUrl, filePath)
+	}
 	wg.Wait()
-	// 返回版本号
-	return versionTag
+
+	// 判断是否是新添加应用
+	if localVersion == "" {
+		fmt.Printf("AddNew: %s (%s)\n", name, releasesTag)
+	} else {
+		fmt.Printf("Update: %s (%s -> %s)\n", name, localVersion, releasesTag)
+		// 删除旧版本文件
+		if !config.Config["dry-run"] && !config.Config["recursive"] {
+			// 对非 "recursive" 的，依次删除旧版本文件
+			for _, templates := range repo.FileList {
+				// 如果模板中不包含 {version} 或 {releases_tag}，则不删除
+				if !strings.Contains(templates, "{version}") && !strings.Contains(templates, "{releases_tag}") {
+					continue
+				}
+				oldFilePath := filepath.Join(fileDir, replaceFileName(localVersion, templates))
+				os.Remove(oldFilePath)
+			}
+		} else if !config.Config["dry-run"] && config.Config["recursive"] {
+			// 对 "recursive" 的，直接删除旧版本目录
+			oldFileDir := filepath.Join(config.OutputDir, releasesDownloadUrl, localVersion)
+			os.RemoveAll(oldFileDir)
+		}
+	}
+	return
 }
